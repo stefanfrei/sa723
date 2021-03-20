89c8b4e49b9730d873ea4b4b5662e2699e618380
Stefan Frei <stefan.a.frei@gmail.com>
2021-03-20 01:59:03 +0100

    drying code

--- src/main/webapp/js/base.js
+++ src/main/webapp/js/base.js
@@ -1,16 +1,20 @@
 $(document).ready(function () {
     const target = document.querySelector('#wrapper');
-    for (let i=0; i<1600; i+=1) {
+    for (i of range(1, 1600)) {
         createDiv(target);
     }
-
     //setInterval(doEffect(target), 500);
 });
+
+function* range(start, end) {
+    for (let i = start; i <= end; i += 1) {
+        yield i;
+    }
+}
 
 createDiv = (target, prepend = false) => {
     const div = document.createElement('DIV');
     div.innerHTML = String.fromCharCode(0x30A0 + Math.random() * (0x30FF-0x30A0+1));
-    console.log(target);
     prepend ? target.prepend(div) : target.append(div);
 }
 
@@ -20,13 +24,13 @@ doEffect = (target) => {
 };
 
 prependLine = (target) => {
-    for (let i = 0; i < 40; i =+ 1) {
+    for (i of range(1, 40)) {
         createDiv(target, true);
     }
 };
 
 flushLastLine = (target) => {
-    for (let i = 0; i < 40; i += 1) {
+    for (i of range(1, 40)) {
         target.lastChild.remove();
     }
 };
