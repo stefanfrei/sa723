fd982cb992003b32bad65787e8190fd7b177f0a2
Stefan Frei <stefan.a.frei@gmail.com>
2021-03-20 08:43:22 +0100

    solid progress

--- src/main/webapp/js/base.js
+++ src/main/webapp/js/base.js
@@ -1,10 +1,9 @@
-$(document).ready(function () {
-    const target = document.querySelector('#wrapper');
-    for (i of range(1, 1600)) {
-        createDiv(target);
-    }
-    //setInterval(doEffect(target), 500);
-});
+const NUM_ROWS = 40;
+const CELLS_PER_ROW = 40;
+const UC_BOUNDS = {
+    lower : 0x30A0,
+    upper : 0x30FF,
+};
 
 function* range(start, end) {
     for (let i = start; i <= end; i += 1) {
@@ -12,11 +11,39 @@ function* range(start, end) {
     }
 }
 
-const createDiv = (target, prepend = false) => {
-    const div = document.createElement('DIV');
-    div.innerHTML = String.fromCharCode(0x30A0 + Math.random() * (0x30FF-0x30A0+1));
-    prepend ? target.prepend(div) : target.append(div);
+const initMatrix = () => {
+    const target = document.querySelector('#wrapper');
+    for (id of range(1, NUM_ROWS)) {
+        target.append(
+            createRow(id),
+        );
+    }
+};
+
+const createRow = (id) => {
+    const row = document.createElement('DIV');
+    row.id = id;
+    row.classList = 'cfx';
+    for (i of range(1, CELLS_PER_ROW)) {
+        row.append(
+            createCell(),
+        );
+    }
+    return row;
 }
+
+const createCell = () => {
+    const cell = document.createElement('DIV');
+    cell.classList = 'cell';
+    cell.innerHTML = createRandomChar();
+    return cell;
+}
+
+const createRandomChar = () => {
+    return String.fromCharCode(
+        UC_BOUNDS.lower + Math.random() * (UC_BOUNDS.upper-UC_BOUNDS.lower+1),
+    );
+};
 
 const doEffect = (target) => {
     flushLastLine(target);
@@ -29,8 +56,10 @@ const prependLine = (target) => {
     }
 };
 
-flushLastLine = (target) => {
+const flushRow = (target) => {
     for (i of range(1, 40)) {
         target.lastChild.remove();
     }
 };
+
+window.addEventListener('DOMContentLoaded', initMatrix);
