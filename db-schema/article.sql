b7f004077eb57d7fb4b16c7c5808b4d1aab34f87
Stefan Frei <stefan.a.frei@gmail.com>
2021-03-30 10:33:58 +0200

    post review

--- db-schema/article.sql
+++ db-schema/article.sql
@@ -1,12 +1,12 @@
 CREATE TABLE "article" (
-	"id" INTEGER NOT NULL DEFAULT 'nextval(''article_id_seq''::regclass)',
-	"id_author" INTEGER NOT NULL,
-	"lang" CHAR(5) NOT NULL,
+    "id" INTEGER NOT NULL DEFAULT 'nextval(''article_id_seq''::regclass)',
+    "id_author" INTEGER NOT NULL,
+    "lang" CHAR(5) NOT NULL,
     "created" TIMESTAMPTZ NOT NULL,
-	"published" TIMESTAMPTZ NOT NULL,
-	"title-short" CHAR(30) NOT NULL,
-	"title" CHAR(50) NOT NULL,
-	"teaser" CHAR(300) NOT NULL,
-	"text" TEXT NOT NULL
+    "published" TIMESTAMPTZ NOT NULL,
+    "title-short" CHAR(30) NOT NULL,
+    "title" CHAR(50) NOT NULL,
+    "teaser" CHAR(300) NOT NULL,
+    "text" TEXT NOT NULL
 )
 ;
